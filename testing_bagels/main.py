d40364efa6ad97becbcb8dc3959764f529473309
aleksisv <aleksis.vuoksenmaa@helsinki.fi>
2018-01-09 23:46:19 +0200

    Modified data structures

--- testing_bagels/main.py
+++ testing_bagels/main.py
@@ -6,46 +6,34 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from sklearn import linear_model
 
-train_data = gen.generate_graphs(300, 40, 100)
-test_data = gen.generate_graphs(100, 40, 100)
+def visualise(x1, x2, x3, y):
+    fig = plt.figure()
+    ax = Axes3D(fig)
+
+    ax.scatter(x1, x2, x3, c = train_data['y'])
+    ax.set_xlabel('Ave. dist.')
+    ax.set_ylabel('Max. eig.gap')
+    ax.set_zlabel('sum eig.val')
+    plt.show()
+
+train_data = gen.generate_graphs(300, 4, 100)
+test_data = gen.generate_graphs(100, 4, 100)
 
 new_train_data = []
 new_test_data = []
-for i in range(len(train_data.index)):
-	new_train_data.append([list(emb.embed(train_data.X[i])), train_data.y[i]])
 
-for i in range(len(test_data.index)):
-	new_test_data.append([list(emb.embed(test_data.X[i])), test_data.y[i]])
+for i in train_data['X']:
+    new_train_data.append(emb.embed(i))
 
-train_data = pand.DataFrame(new_train_data, columns = ['X', 'y'])
-test_data = pand.DataFrame(new_test_data, columns = ['X', 'y'])
-
-train_x1 = []
-train_x2 = []
-train_x3 = []
-for i in range(len(train_data.index)):
-	train_x1.append(train_data['X'][i][0])
-	train_x2.append(train_data['X'][i][1])
-	train_x3.append(train_data['X'][i][2])
-
-test_x1 = []
-test_x2 = []
-test_x3 = []
-for i in range(len(test_data.index)):
-	test_x1.append(test_data['X'][i][0])
-	test_x2.append(test_data['X'][i][1])
-	test_x3.append(test_data['X'][i][2])
+for i in test_data['X']:
+    new_test_data.append(emb.embed(i))
 
 clf = linear_model.Lasso(alpha=0.1)
-clf.fit(np.column_stack((train_x1, train_x2, train_x3)), train_data['y'])
-pred = clf.predict(np.column_stack((test_x1, test_x2, test_x3)))
+
+clf.fit(new_train_data, train_data['y'])
+
+pred = clf.predict(new_test_data)
+
 print("Average error: ", sum(pred-test_data['y'])/len(test_data))
 
-fig = plt.figure()
-ax = Axes3D(fig)
-
-ax.scatter(train_x1, train_x2, train_x3, c = train_data['y'])
-ax.set_xlabel('Ave. dist.')
-ax.set_ylabel('Max. eig.gap')
-ax.set_zlabel('sum eig.val')
-plt.show()
+#visualise(new_train_data, train_data['y'])
