e9ab47406d0efb3552bfe50b22e4f5e77ee717d0
aleksisv <aleksis.vuoksenmaa@helsinki.fi>
2018-01-11 17:21:23 +0200

    Added functions for calculating the laplacian matrix of a given matrix

--- testing_bagels/neur_net.py
+++ testing_bagels/neur_net.py
@@ -7,49 +7,51 @@ import testing_bagels.graphgen as gen
 dimension_signature = [3, 3, 1]
 num_layers = len(dimension_signature)
 
-e = 0.1
+eps = 0.1
 regularity = 0.1
 
-def backpropagate(weights, affines, error):
-    return(weights, affines)
+def backpropagate(weights, biases, error):
+
+    return(weights, biases)
+
+
 
 def train_model(data, labels, num_iterations):
-    model = {'weights':[], 'affines':[]}
+    #Initialising the model with empty weights and bias vectors.
+    model = {'weights':[], 'bias':[]}
 
     weights = model['weights']
-    affines = model['affines']
+    biases = model['bias']
 
+    #Random initial values for weight matrices.
     for i in range(1, num_layers):
         weights.append(np.random.randn(dimension_signature[i-1],
           dimension_signature[i]) / np.sqrt(dimension_signature[i-1]))
-        affines.append(np.zeros((1, dimension_signature[i])))
+        biases.append(np.zeros((1, dimension_signature[i])))
 
 
     for i in range(num_iterations):
-        z_in = []
-        z_out = []
-        z_out.append(data)
+        z = {'z_in':[], 'z_out':[]}
+
+        z['z_out'].append(data)
 
         for i in range(num_layers-1):
-            to_append = np.dot(z_out[i], weights[i])
-            to_append = to_append + affines[i]
-            z_in.append(to_append)
-            z_out.append(np.tanh(to_append))
+            to_append = np.dot(z['z_out'][i], weights[i])
+            to_append = to_append + biases[i]
+            z['z_in'].append(to_append)
+            z['z_out'].append(np.tanh(to_append))
 
-        fin = z_in[num_layers-2]
+        fin = z['z_in'][num_layers-2]
         scores = np.exp(fin)
-        scores = scores[:,0]
-
-        labels = np.asarray([item for sublist in labels for item in sublist])
-
-        error = 0.5 * np.power(scores - labels, 2)
+        diff = scores-labels
+        error = 0.5 * np.power(diff, 2)
 
         print(error)
 
         #TODO Backpropagation part.
-        weights, affines = backpropagate(weights, affines, error)
+        weights, biases = backpropagate(weights, biases, error)
 
-        model = {'weights': weights, 'affines': affines}
+        model = {'weights': weights, 'bias': biases}
 
     return model
 
